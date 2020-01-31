59fc23f3e1300758f0273fec96fea440f5d878c3
sabmeua <sabme.ua@gmail.com>
2020-01-31 17:26:23 +0900

    Change method name according to mruby

--- runtime/lib/lambda_logger.rb
+++ runtime/lib/lambda_logger.rb
@@ -2,7 +2,7 @@ class LambdaLogger
   class << self
     def log_error(exception:, message: nil)
       STDERR.puts message if message
-      STDERR.puts JSON.pretty_unparse(exception.to_lambda_response)
+      STDERR.puts JSON.pretty_generate(exception.to_lambda_response)
     end
   end
 end
